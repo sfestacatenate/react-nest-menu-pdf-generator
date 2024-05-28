62aa1c7a733ad1b609eba3271ae2982dab520844
sfestacatenate <simonefesta1@gmail.com>
2024-05-28 18:41:09 +0200

    Connect to mysql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MenuController } from './menu/menu.controller';
 import { MenuService } from './menu/menu.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '12345',
+      database: 'restaurants',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, MenuController],
   providers: [AppService, MenuService],
 })
---
+++ backend/src/entities/menu.entity.ts
@@ -0,0 +1,17 @@
+import { MenuCategory } from 'src/models/Models';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  lastName: string;
+
+  @Column('json')
+  categories: MenuCategory[];
+}
