a2223e47be0f52fc10eb52bbbdaccb342ecfb682
sfestacatenate <simonefesta1@gmail.com>
2024-06-05 19:58:20 +0200

    Better navigation menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Contact from './pages/Contact/Contact';
 import NavigationBar from './components/NavigationBar';
 import Menus from './pages/Menus/Menus';
 import MenuCreation from './pages/MenuCreation/MenuCreation';
@@ -13,8 +11,6 @@ function App() {
       <NavigationBar />
       <Routes>
         <Route path="/" Component={Home} />
-        <Route path="/about" Component={About} />
-        <Route path="/contact" Component={Contact} />
         <Route path="/menus" Component={Menus} />
         <Route path="/menu-creation" Component={MenuCreation} />
       </Routes>
--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -3,14 +3,20 @@ import { Link } from 'react-router-dom';
 
 const NavigationBar: React.FC = () => {
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/menus">Menus</Link></li>
-                <li><Link to="/menu-creation">Menu Creation</Link></li>
-            </ul>
+        <nav className="bg-blue-600 p-4">
+            <div className="container mx-auto">
+                <ul className="flex flex-col md:flex-row md:space-x-4">
+                    <li className="mb-2 md:mb-0">
+                        <Link className="text-white hover:text-gray-200" to="/">Home</Link>
+                    </li>
+                    <li className="mb-2 md:mb-0">
+                        <Link className="text-white hover:text-gray-200" to="/menus">Menus</Link>
+                    </li>
+                    <li className="mb-2 md:mb-0">
+                        <Link className="text-white hover:text-gray-200" to="/menu-creation">Menu Creation</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
